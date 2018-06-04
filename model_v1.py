6ca31f624b198b35fabc861c172fd0b4a29d7c8d
Nikhil Bhattasali <nikhilxb@stanford.edu>
2018-06-04 14:53:19 -0700

    Done with RelClassifier, need to debug/test

--- model_v1.py
+++ model_v1.py
@@ -1,6 +1,7 @@
 import torch
 import torch.autograd as autograd
 import torch.nn as nn
+import torch.nn.functional as functional
 from torch.nn.utils.rnn import pad_sequence
 
 import utils
@@ -13,7 +14,7 @@ class RelationClassifier(nn.Module):
     where A and B are the two entity mentions in a sentence and C1, C2, C3 are the before, middle, after sequences of
     context words.
     """
-    def __init__(self, vocab, embed_dim):
+    def __init__(self, vocab, embed_dim, output_dim=230, dropout=0.5, num_labels=50):
         """
         Args:
             vocab = dict[word] -> numpy array(embed_dim,) = vocabulary dict
@@ -22,17 +23,18 @@ class RelationClassifier(nn.Module):
         super(RelationClassifier, self).__init__()
         self.vocab = vocab
         for word, vec in self.vocab.items():
-            self.vocab[word] = torch.from_numpy(vec)
+            self.vocab[word] = torch.FloatTensor(vec)
         self.embed_dim = embed_dim
         self.unk = torch.rand(self.embed_dim)
 
-        self.pcnn = PiecewiseCNN(self.embed_dim, )
+        self.pcnn = PiecewiseCNN(self.embed_dim, output_dim)
+        self.drop1 = nn.Dropout(p=dropout)
+        self.lin1 = nn.Linear(3 * output_dim, num_labels)
 
-    def forward(self, X, y):
+    def forward(self, X):
         """
         Args:
             X = list[ tuple(c1, c2, c3), ... ] = batch of training examples
-            y = list[ int, ... ]               = relation label indices
             c1/c2/c3 = list[ string, ... ]     = list of word strings
         Return:
             FloatTensor(batch_size, num_labels)
@@ -44,7 +46,11 @@ class RelationClassifier(nn.Module):
         C3 = pad_sequence([self._assemble_vec_seq(c) for c in C3], batch_first=True).transpose(1, 2)
         # output each is FloatTensor(batch_size, embed_dim, max_piece_len)
 
-        h = self.pcnn(C1, C2, C3)
+        h = self.pcnn(C1, C2, C3)  # (batch_size, output_dim, 3)
+        batch_size = h.size()[0]
+        h = h.view(batch_size, -1)  # (batch_size, 3 * output_dim)
+        h = self.drop1(h)
+        h = self.lin1(h)
         return h
 
     def _assemble_vec_seq(self, seq):
@@ -61,7 +67,7 @@ class PiecewiseCNN(nn.Module):
     """Applies convolution over a variable number of pieces and concatenates all the output pieces."""
     def __init__(self,
                  input_dim,
-                 output_dim=230,
+                 output_dim,
                  kernel_size=3,
                  padding=2):
         super(PiecewiseCNN, self).__init__()
@@ -77,6 +83,7 @@ class PiecewiseCNN(nn.Module):
         def process(x):  # (batch_size, input_dim, piece_len)
             x = self.conv1(x)  # (batch_size, output_dim, piece_len)
             x, _ = x.max(dim=2)  # (batch_size, output_dim)
+            x = torch.tanh(x)
             return x
 
         processed = [process(piece) for piece in pieces]
@@ -89,10 +96,16 @@ if __name__ == "__main__":
     x2 = torch.rand(3, 5, 2, requires_grad=True)
     pcnn = PiecewiseCNN(input_dim=5, output_dim=10)
     out = pcnn(x1, x2)  # (batch_size, output_dim, num_pieces)
-    print("PiecewiseCNN test:", out.size() == (3, 10, 2))
+    print("PiecewiseCNN test:")
+    print("--- out.size() == (3, 10, 2):", out.size() == (3, 10, 2))
+    print()
 
     # Test RelationClassifier
     vocab = utils.glove2dict("data/glove.6B.50d.txt")  # dict[word] -> numpy array(embed_dim,)
     rc = RelationClassifier(vocab, 50)
+    X = [ (["first", "piece"], ["second", "piece"], ["third", "piece"]) ]
+    y = [ 0 ]
     print("RelationClassifier test:")
-    print("--- _assemble_vec_seq:", rc._assemble_vec_seq(['apple', 'banana', 'coconut', 'durian', 'apple'])[:, :3])
+    print("--- _assemble_vec_seq:\n", rc._assemble_vec_seq(['apple', 'banana', 'coconut', 'durian', 'apple'])[:, :3])
+    out = rc(X, y)
+    print("--- forward:\n", out.size() )
